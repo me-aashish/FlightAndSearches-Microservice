e1a538a2b8c7b172d6e84310c72b244cfd87add5
Aashish Kumar Jha <20BCS3872@cuchd.in>
2023-03-23 11:06:17 +0530

    created database and several folders using sequelize and added new infos to the README.md

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ const express = require("express")
 require('dotenv').config()
 const { PORT } = require('./config/serverConfig')
 
+const bodyParser = require('body-parser')
 
 
 const setUpAndStartServer = async ()=>{
 
     const app = express();
     const PORT = process.env.PORT
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended : true}))
 
     app.listen(PORT,()=>{
         console.log(`Server is listening on ${PORT}`);
