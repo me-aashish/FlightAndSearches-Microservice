8b20d84aa9a3ef74d47bff6f6c96174e3dfe2287
Aashish Kumar Jha <20BCS3872@cuchd.in>
2023-04-03 19:01:31 +0530

    changed added instructions in readme.md, undoed and then did some db migrations, created repository layer and its codin

---
+++ src/repository/city-repository.js
@@ -0,0 +1,66 @@
+const { City } = require('../models/index')
+
+class CityRepository{
+
+    // constructor(){
+
+    // }
+
+    async createCity({ name }){
+        try {
+            const city = await City.create({
+                name
+            })
+        } catch (error) {
+            console.log("Something went wrong in repository layer");
+            throw {error};
+        }
+    }
+
+    async deleteCity(cityId){
+
+        try {
+            await City.destroy({
+                where :{
+                    id : cityId
+                }
+            })
+            return true;
+        } catch (error) {
+            console.log("Something went wrong in repository layer");
+            throw {error};
+        }
+        
+    }
+
+    async updateCity(cityId,data){
+
+        try {
+            const city = City.update(data,{
+                where:{
+                    id : cityId
+                }
+            })
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in repository layer");
+            throw {error};
+        }
+
+    }
+
+    async getCity(cityId){
+
+        try {
+            const city = await City.findByPk(cityId);
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in repository layer");
+            throw {error};
+        }
+
+    }
+
+}
+
+module.exports = CityRepository
